c382173890df3f62b6d3d639b48287a1939ea826
Petr Petrenko <ppetrenko@xiag.ch>
2018-03-19 02:41:48 +0700

    fix

--- bin/console.php
+++ bin/console.php
@@ -3,10 +3,12 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use \AVAllAC\PersistentBlockStorage\Command\CheckVolumeCommand;
+
 $pimple = new Pimple\Container();
 $pimple['config'] = \Symfony\Component\Yaml\Yaml::parseFile(__DIR__ . '/../etc/config.yml');
 $pimple->register(new \AVAllAC\PersistentBlockStorage\Provider\ServerStorageManagerProvider());
 
 $application = new \Symfony\Component\Console\Application();
-$application->add(new \AVAllAC\PersistentBlockStorage\Command\CheckVolumeCommand(null, $pimple['storageManager']));
+$application->add(new CheckVolumeCommand(null, $pimple['serverStorageManager']));
 $application->run();
